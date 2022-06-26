09a66b3832179a4cbbe20d5cb259d26c02ceaaf8
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-06-26 21:29:54 +0500

    ok

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,15 +5,14 @@ using System.Web;
 
 namespace Inventory_Management_System.Models
 {
-    public class Items
+    public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string desc { get; set; }
         public string price { get; set; }
-        public virtual User Seller { get; set; }
-        public int SellerId { get; set; }
+        //public virtual User Seller { get; set; }
+        //public int SellerId { get; set; }
         public int quantity { get; set; }
-
     }
 }
